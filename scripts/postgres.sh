ed17e1a13bdf6e07f6d3fa322101757dd9a596e6
sonia <sonia2@milevas-m01.vmware.com>
2018-01-16 23:17:15 +0200

    Fix owners of the tables in Postgres shell script

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -4,4 +4,6 @@ sudo -u postgres psql -c "CREATE USER alpralpr WITH PASSWORD 'alpralpr'"
 sudo -u postgres psql -c "CREATE DATABASE alpralpr OWNER alpralpr"
 echo "\c alpralpr;
 CREATE TABLE violation_record (picture bytea, license_plate_number character varying(128), captured_date timestamp with time zone, recorded_date timestamp with time zone, PRIMARY KEY(license_plate_number, captured_date));
-CREATE TABLE permitted_vehicles (license_plate_number character varying(128), type character varying(128), PRIMARY KEY(license_plate_number));" |sudo -u postgres psql
+ALTER TABLE violation_record OWNER TO alpralpr;
+CREATE TABLE permitted_vehicles (license_plate_number character varying(128), type character varying(128), PRIMARY KEY(license_plate_number));
+ALTER TABLE permitted_vehicles OWNER TO alpralpr;" |sudo -u postgres psql
