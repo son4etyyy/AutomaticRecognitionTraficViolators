2e7bf16234bef30ce250ddb918437ff45dc614f9
Sonia <son4etyyy@gmail.com>
2018-01-16 08:14:04 +0200

    Make setup of openalpr happens only id it doesn't exist

--- Server/scripts/openalpr.sh
+++ Server/scripts/openalpr.sh
@@ -1,13 +1,15 @@
-sudo apt-get install libleptonica-dev -y
-sudo apt-get install cmake -y
-sudo apt-get install libtesseract-dev -y
-sudo apt-get install libopencv-dev -y
-sudo apt-get install libcurl4-openssl-dev -y
- sudo apt-get install liblog4cplus-dev -y
-git clone https://github.com/openalpr/openalpr.git src/main/resources/openalpr
-cd src/main/resources/openalpr/src
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_INSTALL_SYSCONFDIR:PATH=/etc ..
-make
-sudo make install
+if [ ! -d "src/main/resources/openalpr" ]; then
+    sudo apt-get install libleptonica-dev -y
+    sudo apt-get install cmake -y
+    sudo apt-get install libtesseract-dev -y
+    sudo apt-get install libopencv-dev -y
+    sudo apt-get install libcurl4-openssl-dev -y
+    sudo apt-get install liblog4cplus-dev -y
+    git clone https://github.com/openalpr/openalpr.git src/main/resources/openalpr
+    cd src/main/resources/openalpr/src
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_INSTALL_SYSCONFDIR:PATH=/etc ..
+    make
+    sudo make install
+fi
